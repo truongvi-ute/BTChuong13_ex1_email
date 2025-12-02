f579a5c0ed451b98e5fdde26d6ccdf543ecdf3f2
Truong Vi <nguyendoantruongvi11@gmail.com>
2025-12-02 14:00:38 +0700

    second cmm

--- build/generated-sources/ap-source-output/murach/business/User_.java
+++ build/generated-sources/ap-source-output/murach/business/User_.java
@@ -4,7 +4,7 @@ import javax.annotation.processing.Generated;
 import javax.persistence.metamodel.SingularAttribute;
 import javax.persistence.metamodel.StaticMetamodel;
 
-@Generated(value="org.eclipse.persistence.internal.jpa.modelgen.CanonicalModelProcessor", date="2025-12-02T00:15:51", comments="EclipseLink-2.7.12.v20230209-rNA")
+@Generated(value="org.eclipse.persistence.internal.jpa.modelgen.CanonicalModelProcessor", date="2025-12-02T13:59:15", comments="EclipseLink-2.7.12.v20230209-rNA")
 @StaticMetamodel(User.class)
 public class User_ { 
 
