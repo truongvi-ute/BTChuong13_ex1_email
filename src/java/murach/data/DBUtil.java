66c5a739b9f6c3d888ae652d8c8df471ffa8aaca
Truong Vi <nguyendoantruongvi11@gmail.com>
2025-12-02 15:17:06 +0700

    done

--- src/java/murach/data/DBUtil.java
+++ src/java/murach/data/DBUtil.java
@@ -1,39 +1,13 @@
 package murach.data;
 
-import java.util.HashMap;
-import java.util.Map;
 import javax.persistence.EntityManagerFactory;
 import javax.persistence.Persistence;
 
 public class DBUtil {
     
-    private static final EntityManagerFactory emf;
+    private static final EntityManagerFactory emf =
+            Persistence.createEntityManagerFactory("emailListPU");
     
-    static {
-        Map<String, String> properties = new HashMap<>();
-        
-        String dbUrl = System.getenv("DB_URL");
-        String dbUser = System.getenv("DB_USER");
-        String dbPass = System.getenv("DB_PASS");
-
-        if (dbUrl != null && !dbUrl.isEmpty()) {
-            // --- ĐOẠN CODE SỬA ĐỔI Ở ĐÂY ---
-            // Kiểm tra: Nếu URL từ Render chưa có "jdbc:postgresql://" thì mới thêm vào.
-            // Nếu có rồi (như trường hợp của bạn) thì giữ nguyên.
-            String finalUrl = dbUrl;
-            if (!dbUrl.startsWith("jdbc:postgresql://")) {
-                finalUrl = "jdbc:postgresql://" + dbUrl;
-            }
-            // -------------------------------
-
-            properties.put("javax.persistence.jdbc.url", finalUrl);
-            properties.put("javax.persistence.jdbc.user", dbUser);
-            properties.put("javax.persistence.jdbc.password", dbPass);
-        }
-        
-        emf = Persistence.createEntityManagerFactory("emailListPU", properties);
-    }
-
     public static EntityManagerFactory getEmFactory() {
         return emf;
     }
